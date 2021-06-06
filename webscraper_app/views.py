c22b271f7da997d75ca96971d185bd45a03126f5
lahiru-k <slrupees@gmail.com>
2021-06-06 12:18:42 +0530

    added frontend and requirements.txt

--- webscraper_app/views.py
+++ webscraper_app/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from bs4 import beautifulsoup
 
 def home(request):
     return render(request, "base.html", )
 
 def new_search(request):
-    return render(request, "content/new_search.html", )
+    search_data = request.POST.get('search')
+    print(search_data)
+    context = {
+        'search_term' : search_data,
+    }
+    return render(request, "content/new_search.html", context,)
